6f6573d74ea8aa3f8a34ae18acd8c84e95680417
Jay Nair <jnair@munichre.com>
2025-04-04 15:23:26 +0200

    updates

--- init.lua
+++ init.lua
@@ -24,11 +24,47 @@ vim.g.maplocalleader = "\\"
 -- Set editor options
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard for yank/paste
+vim.opt.tabstop = 2       -- Number of visual spaces per TAB
+vim.opt.softtabstop = 2   -- Number of spaces TAB counts for in insert mode
+vim.opt.shiftwidth = 2    -- Number of spaces to use for autoindent
+vim.opt.expandtab = true  -- Use spaces instead of tabs
+
+-- -----------------------------------------------------------------------------
+-- Custom Options Section
+-- -----------------------------------------------------------------------------
+
+-- Enhanced Searching
+vim.opt.hlsearch = true -- Highlight all search matches
+vim.opt.incsearch = true -- Show search matches incrementally as you type
+-- vim.opt.ignorecase = true -- Ignore case when searching
+vim.opt.smartcase = true -- Override ignorecase if search pattern has uppercase letters
+
+-- Smoother Scrolling
+vim.opt.scrolloff = 8 -- Keep 8 lines visible above/below cursor when scrolling
+vim.opt.sidescrolloff = 8 -- Keep 8 columns visible left/right of cursor when scrolling horizontally
+
+-- Persistent Undo
+vim.opt.undofile = true -- Enable persistent undo
+local undodir = vim.fn.stdpath('data') .. '/undodir'
+-- Ensure the directory exists
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, 'p') -- Create parent directories if needed
+end
+vim.opt.undodir = undodir -- Set the undo directory
+
+-- Better Command Line Experience
+vim.opt.cmdheight = 1 -- Set command line height
+vim.opt.showmode = false -- Hide default mode indicator (status line usually shows it)
+
+-- -----------------------------------------------------------------------------
+-- End Custom Options Section
+-- -----------------------------------------------------------------------------
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    -- add your plugins here
-    -- 'folke/tokyonight.nvim',
+  -- add your plugins here
+  -- 'folke/tokyonight.nvim',
 
   spec = {
     -- Theme
@@ -58,19 +94,11 @@ require("lazy").setup({
       event = { "BufReadPost", "BufNewFile" },
       build = ":TSUpdate", -- Command to install/update parsers
       config = function()
+        -- Basic Treesitter setup
         require('nvim-treesitter.configs').setup {
           highlight = { enable = true },
           indent = { enable = true },
-          incremental_selection = {
-            enable = true,
-            keymaps = {
-              init_selection = '<c-space>', -- Start selection
-              node_incremental = '<c-space>', -- Increment selection
-              scope_incremental = '<c-s>', -- Select scope (not commonly used)
-              node_decremental = '<bs>', -- Decrement selection
-            },
-          },
-          -- Add other Treesitter modules here as needed (e.g., textobjects, autotag)
+          -- Other modules can be added here if needed later
         }
       end,
     },
@@ -83,15 +111,7 @@ require("lazy").setup({
     {
       'mhinz/vim-signify',
       event = { "BufReadPost", "BufNewFile" }, -- Git diff signs, load on file open/create
-      config = function()
-        -- Ensure Signify is enabled when the plugin loads and a buffer is entered
-        vim.cmd([[
-          augroup signify_auto_enable
-            autocmd!
-            autocmd BufEnter * SignifyEnable
-          augroup END
-        ]])
-      end,
+      -- Rely on default behavior and events to enable signify
     },
     { 'jalvesaq/Nvim-R', ft = { "r", "rmd", "quarto" } }, -- Load on R related filetypes
     { 'idbrii/vim-mergetool', cmd = "Mergetool" },
@@ -132,26 +152,61 @@ require("lazy").setup({
         })
       end,
     },
+    -- File Explorer
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- Recommended for icons
+        "MunifTanjim/nui.nvim",
+      }
+    },
 
     -- UI
+
+
+
+    {
+      "folke/persistence.nvim",
+      event = "BufReadPre", -- Load session management early
+      opts = { -- Options for session saving
+        options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" }
+      },
+      config = true -- Explicitly run setup with opts
+    },
+    { "folke/flash.nvim", event = "VeryLazy", opts = {} }, -- Enhanced jump motions
     {
       'goolord/alpha-nvim',
       event = 'VimEnter',
       dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Optional: for icons
-      config = function()
-        require('alpha').setup(require('alpha.themes.dashboard').config)
-      end,
+      config = function ()
+        -- Use default alpha dashboard theme
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+      end
     },
     { 'rebelot/heirline.nvim', event = "VeryLazy", dependencies = { 'nvim-tree/nvim-web-devicons' } }, -- Load UI later
     {
       'nvim-lualine/lualine.nvim',
       event = "VeryLazy",
       dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+        require('lualine').setup()
+      end,
     },
     {
       'nvim-telescope/telescope.nvim',
-      cmd = "Telescope",
+      event = "VimEnter", -- Load Telescope at startup
       dependencies = { 'nvim-lua/plenary.nvim' },
+      config = function()
+        -- Add Telescope keymaps
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope recent files (MRU)' }) -- Added recent/MRU files
+
+      end,
     },
     {
       "folke/which-key.nvim",
@@ -159,6 +214,17 @@ require("lazy").setup({
       config = function()
         require("which-key").setup({}) -- Add setup call if needed later
       end,
+    },
+
+    -- File Explorer
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- Recommended for icons
+        "MunifTanjim/nui.nvim",
+      }
     },
 
     -- Diagnostics / Trouble
@@ -190,11 +256,45 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { "dracula" } },
   -- automatically check for plugin updates
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "dracula" } }, -- Set install colorscheme
+  -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
 -- Set the colorscheme after lazy setup
-vim.cmd([[colorscheme habamax]]) 
+vim.cmd([[colorscheme dracula]]) -- Set Dracula theme
 
 -- Alias LspInstall to MasonInstall for convenience
 vim.api.nvim_create_user_command('LspInstall', 'MasonInstall', {})
+
+-- Keymaps can be defined here or in separate files
+-- vim.defer_fn(function() ... end, 0) -- Removed Telescope keymaps previously added for default alpha
+
+-- Neo-tree toggle keymap
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Explorer NeoTree", noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>ev', function()
+  vim.cmd.edit(vim.fn.stdpath('config') .. '/init.lua')
+end, { desc = 'Edit Neovim config (init.lua)' })
+
+vim.keymap.set('n', '<leader>R', '<cmd>source $MYVIMRC<CR>', { desc = 'Reload Neovim config', noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>S', function()
+  require('persistence').load()
+end, { desc = 'Load last session' })
+
+-- Buffer navigation shortcuts
+vim.keymap.set('n', ']b', ':bnext<CR>', { desc = 'Next buffer', silent = true })
+vim.keymap.set('n', '[b', ':bprevious<CR>', { desc = 'Previous buffer', silent = true })
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Explorer NeoTree", noremap = true, silent = true })
+
+-- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
