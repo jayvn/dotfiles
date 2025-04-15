2b8d0874ec0fe626da8b2d2174beb6192c57621d
Jay Nair <jnair@munichre.com>
2025-04-15 09:16:15 +0200

    cool ! treesitter syntax highlighting finally

--- init.lua
+++ init.lua
@@ -213,7 +213,11 @@ require("lazy").setup({
       build = ":TSUpdate", 
       config = function()
         require('nvim-treesitter.configs').setup {
-          highlight = { enable = true },
+          ensure_installed = {"python", "lua", "vim", "r" ,"bash","yaml"," markdown"},
+          highlight = {
+            enable = true ,
+            additional_vim_regex_highlighting = false,
+          },
           indent = { enable = true },
         }
       end,
