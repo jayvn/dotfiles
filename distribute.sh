d5aef30621c31544e24f3a3f37e83a4136c2462d
Jay <jay@somewhere.com>
2025-08-10 12:43:44 +0200

    fixes

--- distribute.sh
+++ distribute.sh
@@ -23,27 +23,32 @@ filelocs=(
     .zshrc
     .gitconfig)
 
+
+
+# Setup vim-plug for Vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 #===
 # steam deck nix pkg nvim
 curl -L https://nixos.org/nix/install | sh
-nix-env -i neovim
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
+nix-env -if nix-packages.nix
 #===
 
 # --- Sometimes you have conda installed stuff that just isn't picked up by other tools (neovim in these cases)
-# ln -s /home/jay/miniconda/bin/node /home/jay/.local/bin/ # uv tool install nodeenv? bun? 
+# ln -s /home/jay/miniconda/bin/node /home/jay/.local/bin/ 
 # ---
 
-#TODO: If .vim does not exist, create it
+# Setup separate configs for Vim and Neovim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -sf ~/.vim $XDG_CONFIG_HOME/nvim
-ln -sf ${loc}/init.vim $XDG_CONFIG_HOME/nvim/init.vim
+mkdir -p ~/.vim
+mkdir -p $XDG_CONFIG_HOME/nvim
+
+# Vim uses init.vim as ~/.vimrc
 ln -sf ${loc}/init.vim ~/.vimrc
 
-ln -s ${loc}/user.lua $XDG_CONFIG_HOME/nvim/lua/plugins/user.lua
+# Neovim uses init.lua in its config directory
+ln -sf ${loc}/init.lua $XDG_CONFIG_HOME/nvim/init.lua
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
