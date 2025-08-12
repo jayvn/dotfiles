3c3a74b045f8838ab88bdfb56c698216d5516e1b
Jay Nair <jnair@munichre.com>
2025-08-12 11:34:54 +0200

    minor update

--- nix-packages.nix
+++ nix-packages.nix
@@ -3,7 +3,8 @@
 pkgs.buildEnv {
   name = "my-packages";
   paths = with pkgs; [
-    # zsh # not doing this cuz everyone looks at /bin/zsh
+    # zsh # not doing this cuz everyone looks at /bin/zsh not nix folder
+    atuin
     bat
     btop
     bun
@@ -11,23 +12,22 @@ pkgs.buildEnv {
     difftastic
     direnv
     eza
-    firefox
+    # firefox
     fzf
     lazygit
     neovim
-    openblas
+    nushell
+    # openblas
     ripgrep
     ruff
     shfmt
     tmux
     universal-ctags
     vim
-    vscode
+    # vscode #Non free, not needed for wsl
     yazi
+    zellij
     zig
     zoxide
-    # zellig
-    atuin
-    nushell
   ];
 }
