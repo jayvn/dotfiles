c33e61810a8c6d98557563708c3bd93ca647c0de
Jay Nair <jnair@munichre.com>
2025-08-19 17:45:53 +0200

    redundant config

--- init.lua
+++ init.lua
@@ -320,22 +320,6 @@ require("lazy").setup({
 							move = {
 								enable = true,
 								set_jumps = true,
-								goto_next_start = {
-									["]m"] = "@function.outer",
-									["]]"] = "@class.outer",
-								},
-								goto_next_end = {
-									["]M"] = "@function.outer",
-									["]["] = "@class.outer",
-								},
-								goto_previous_start = {
-									["[m"] = "@function.outer",
-									["[["] = "@class.outer",
-								},
-								goto_previous_end = {
-									["[M"] = "@function.outer",
-									["[]"] = "@class.outer",
-								},
 							},
 						},
 					},
